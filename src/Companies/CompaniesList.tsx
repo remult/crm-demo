6038f1da17357659828c66839759fab4f2a6d51f
Yoni Rapoport <yoni@fireflymigration.com>
2022-01-27 12:06:56 +0200

    companies list virtualized infinite scroll

--- src/Companies/CompaniesList.tsx
+++ src/Companies/CompaniesList.tsx
@@ -1,5 +1,5 @@
 import { Button, Grid, IconButton, List, ListItem, ListItemButton, ListItemText, Stack, TextField } from "@mui/material";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { remult } from "../common"
 import { Company } from "./Company.entity"
 import AddIcon from '@mui/icons-material/Add';
@@ -11,6 +11,9 @@ import { CompanySize } from "./CompanySize";
 import CancelIcon from '@mui/icons-material/Cancel';
 import { sectors } from "./Sectors";
 import { Link } from 'react-router-dom';
+import InfiniteLoader from "react-window-infinite-loader";
+import { FixedSizeList } from "react-window";
+import { Paginator } from "remult";
 
 
 const amRepo = remult.repo(Company);
@@ -26,24 +29,41 @@ export const CompaniesList: React.FC<{}> = () => {
         setSearchParams({ ...filter, ...f });
     }
 
-    const [companies, setCompanies] = useState<Company[]>([]);
-    const loadCompanies = useCallback(() => amRepo.find({
-        where: {
-            name: { $contains: filter.search },
-            size: filter.size ? CompanySize.helper.byId(filter.size) : undefined,
-            sector: filter.sector ? filter.sector : undefined
-        }, limit: 5
-    }).then(setCompanies), [filter.search, filter.size, filter.sector]);
+    const [companies, setCompanies] = useState<{ companies: Company[], paginator?: Paginator<Company> }>({
+        companies: []
+    });
+
     useEffect(() => {
-        loadCompanies()
-    }, [loadCompanies]);
+        (async () => {
+            const paginator = await amRepo.query({
+                where: {
+                    name: { $contains: filter.search },
+                    size: filter.size ? CompanySize.helper.byId(filter.size) : undefined,
+                    sector: filter.sector ? filter.sector : undefined
+                }, pageSize: 50
+            }).paginator();
+            setCompanies({ companies: paginator.items, paginator: paginator });
+        })();
+    }, [filter.search, filter.size, filter.sector]);
+
+    const itemCount = companies.paginator ? (companies.paginator.hasNextPage ? companies.companies.length + 1 : companies.companies.length) : 0;
+
+    const loadMoreItems = async () => {
+        if (companies.paginator) {
+            const newPaginator = await companies.paginator.nextPage();
+            setCompanies({ companies: [...companies.companies, ...newPaginator.items], paginator: newPaginator });
+        }
+    }
+
+    const isItemLoaded = (index: number) => !!companies.paginator && (!companies.paginator.hasNextPage || index < companies.companies.length);
+
     const [editCompany, setEditCompany] = useState<Company>();
     const deleteCompany = async (deletedCompany: Company) => {
         await amRepo.delete(deletedCompany);
-        setCompanies(companies.filter(company => deletedCompany.id != company.id));
+        setCompanies({ companies: companies.companies.filter(company => deletedCompany.id != company.id), paginator: companies.paginator });
     }
     const editCompanySaved = (editCompany: Company) =>
-        setCompanies(companies.map(company => company.id === editCompany.id ? editCompany : company));
+        setCompanies({ companies: companies.companies.map(company => company.id === editCompany.id ? editCompany : company), paginator: companies.paginator });
 
     return <Grid container spacing={2}>
         <Grid item xs={2}>
@@ -107,25 +127,49 @@ export const CompaniesList: React.FC<{}> = () => {
                 Add Company
             </Button>
             <List>
-                {companies.map(company => (
-                    <ListItem disablePadding key={company.id} secondaryAction={
-                        <Stack direction="row" spacing={2}>
-                            <IconButton edge="end" aria-label="edit"
-                                onClick={() => deleteCompany(company)}>
-                                <DeleteIcon />
-                            </IconButton>
-                            <IconButton edge="end" aria-label="edit"
-                                onClick={() => setEditCompany(company)}>
-                                <EditIcon />
-                            </IconButton>
-                        </Stack>
-                    }>
-                        <ListItemButton component={Link} to={`/companies/${company.id}`}>
-                            <ListItemText primary={company.name} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-
+                {companies.paginator ? (<
+                    InfiniteLoader
+                    isItemLoaded={isItemLoaded}
+                    itemCount={itemCount}
+                    loadMoreItems={loadMoreItems}
+                >
+                    {({ onItemsRendered, ref }) => (
+                        <FixedSizeList
+                            className="List"
+                            height={1000}
+                            itemCount={itemCount}
+                            itemSize={50}
+                            onItemsRendered={onItemsRendered}
+                            ref={ref}
+                            width={"100%"}
+                        >
+                            {({ index, style }) => {
+                                if (!isItemLoaded(index)) {
+                                    return <div style={style}>Loading...</div>
+                                }
+                                else {
+                                    const company = companies.companies[index];
+                                    return <div style={style}><ListItem disablePadding key={company.id} secondaryAction={
+                                        <Stack direction="row" spacing={2}>
+                                            <IconButton edge="end" aria-label="edit"
+                                                onClick={() => deleteCompany(company)}>
+                                                <DeleteIcon />
+                                            </IconButton>
+                                            <IconButton edge="end" aria-label="edit"
+                                                onClick={() => setEditCompany(company)}>
+                                                <EditIcon />
+                                            </IconButton>
+                                        </Stack>
+                                    }>
+                                        <ListItemButton component={Link} to={`/companies/${company.id}`}>
+                                            <ListItemText primary={company.name} />
+                                        </ListItemButton>
+                                    </ListItem></div>
+                                }
+                            }}
+                        </FixedSizeList>
+                    )}
+                </InfiniteLoader>) : null}
             </List>
             {
                 editCompany && <CompanyEdit
